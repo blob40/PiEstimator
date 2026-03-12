b23c9b8adae2d644d7bf93fee3387c2fcce1b689
blob40 <alexandrialax40@gmail.com>
2026-03-12 16:29:00 +0000

    done

--- workspace/PiEstimator.java
+++ workspace/PiEstimator.java
@@ -8,25 +8,20 @@ import java.awt.event.MouseListener;
 
 public class PiEstimator extends Thread {
 	static Thread runner;
+	static Boolean running;
 	static int Tottrials = 0;
 	static int trials = 0;
 	static double piNum;
 	static JFrame f = new JFrame("Button Example");
-	static	JButton P = new JButton();
-	static	JLabel example = new JLabel("Actual Value " + Double.toString(Math.PI));
-	static	JLabel ceL = new JLabel ();
-	static	JLabel trialL = new JLabel();
+	static JButton P = new JButton();
+	static JLabel example = new JLabel("Actual Value " + Double.toString(Math.PI));
+	static JLabel ceL = new JLabel();
+	static JLabel trialL = new JLabel();
 	// the following code is just to jog your memory about how labels and buttons
 	// work!
 	// implement your Pi Estimator as described in the project. You may do it all in
 	// main below or you
 	// may implement additional functions if you feel it necessary.
-
-	public PiEstimator (){
-
-
-     
-	}
 
 	public static void main(String[] args) {
 		PiEstimator pi = new PiEstimator();
@@ -34,7 +29,7 @@ public class PiEstimator extends Thread {
 		pi.start();
 
 		P.setText("Pause");
-		ceL.setText("Current Estimate " + piNum);
+		ceL.setText("Current Estimate " + 0);
 		trialL.setText("Total Trials " + 0);
 		f.add(example);
 		f.add(ceL);
@@ -44,18 +39,38 @@ public class PiEstimator extends Thread {
 		f.setLayout(new GridLayout(4, 1));
 		f.setVisible(true);
 
+		P.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 
-		
-	}
+			if (P.getText() == "Pause"){
+				running = false;
+				P.setText("Run");
+				//notify();
+			} else if (P.getText() == "Run"){
+				running = true;
+				P.setText("Pause");
+				//notify();
+			}
 
-	public void update (){
-		ceL.setText("Current estimate " + piNum);
+				synchronized(pi) {
+					pi.notify();
+				}
+				//if (running = true){
+				//	P.setText("Run");
+				//} else {
+				//	P.setText("Pause");
+				//}
+				System.out.println("Shouldve stopped" + running);
+			}
+		});
+
 	}
 
 	public synchronized void run() {
+
 		
-		Boolean running;
-		
+
 		synchronized (runner) {
 			running = true;
 			runner.notify();
@@ -69,28 +84,23 @@ public class PiEstimator extends Thread {
 					} catch (InterruptedException e) {
 					}
 				}
-			
-			double x = Math.random();
-			double y = Math.random();
-			Tottrials++;
-			//System.out.println("total + " + Tottrials);
-			if((x*x+y*y) <= 1){
-				trials++;
-				piNum = 4.0*(trials)/Tottrials;
-				
-				//mod %mil
-				if (Tottrials%100000 == 0){
-					System.out.println("update");
-				update();
-			}
 
-			trialL.setText("Total Trials " + Tottrials);
-			//System.out.println("Not update");
-				//System.out.println("tot mod = " + Tottrials%1000000);
-				//System.out.println("Trials + " + trials + " pi num + " + piNum);
+				double x = Math.random();
+				double y = Math.random();
+				Tottrials++;
+				trialL.setText("Total Trials " + Tottrials);
+				System.out.println("total + " + Tottrials);
+				if ((x * x + y * y) <= 1) {
+					trials++;
+					piNum = 4.0 * (trials) / Tottrials;
+
+				}
+
+				if (Tottrials % 50000 == 0) {
+						ceL.setText("Current estimate " + piNum);
+					}
+				// rest of your code actually doing stuff here
 			}
-			// rest of your code actually doing stuff here
-		}
 		}
 
 	}
